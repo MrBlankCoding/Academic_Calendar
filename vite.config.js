0171c816d2d016447f3fe3d46538f6b1530c390b
ethanzhall <hallethan156@gmail.com>
2025-09-25 12:40:42 -0400

    Suppport for rendewr

--- vite.config.js
+++ vite.config.js
@@ -24,9 +24,11 @@ export default defineConfig({
     host: true, // bind to 0.0.0.0
     port: Number(process.env.PORT) || 5173,
     open: '/index.html',
+    allowedHosts: ['academic-calendar.onrender.com'],
   },
   preview: {
     host: true, // bind to 0.0.0.0
     port: Number(process.env.PORT) || 5173,
+    allowedHosts: ['academic-calendar.onrender.com'],
   },
 });
