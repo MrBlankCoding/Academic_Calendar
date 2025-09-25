51b2417c0a7614afb1b7c6c0e9b82942baec9ac5
ethanzhall <hallethan156@gmail.com>
2025-09-25 12:43:57 -0400

    Title fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from 'path';
 export default defineConfig({
   root: 'app',
   base: '/',
-  build: {
+    build: {
     outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
